80177b97b01ea0b914f11fc8679d73b25a88ef5a
Machinery <geroorganist@gmail.com>
2023-03-09 19:05:05 -0800

    Write a function that returns the value of x raised to the power of y.

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - This function returns the value of x raised to the power of y
+ * _pow_recursion - This function returns 
+ * the value of x raised to the power of y
  * @x: value to raise
  * @y: power
  *
